1c24b1dbe7fcdabd234f3463aea3dcf274143bda
Jatin sharma <jatin2010sharma@gmail.com>
2026-07-19 18:08:38 +0530

    adding code 2

---
+++ provider/provider/src/main/java/com/provider/provider/HelloController.java
@@ -0,0 +1,13 @@
+package com.provider.provider;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello from Provider 2";
+    }
+}
